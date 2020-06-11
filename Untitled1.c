bd852196cdaee05373167c37ca3e822ade35e92d
RashadTanjim <rashad.tanjim@northsouth.edu>
2020-06-12 05:42:43 +0600

    Extra files added

---
+++ Untitled1.c
@@ -0,0 +1,272 @@
+#include<windows.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc                   //contains toupper(), tolower(),etc
+#include<dos.h>                     //contains _dos_getdate
+#include<time.h>
+
+#define RETURNTIME 15
+
+char catagories[][15]={"Computer","Electronics","Electrical","Civil","Mechnnical","Architecture"};
+void returnfunc(void);
+void mainmenu(void);
+void addbooks(void);
+void deletebooks(void);
+void editbooks(void);
+void searchbooks(void);
+void issuebooks(void);
+void viewbooks(void);
+void closeapplication(void);
+int  getdata();
+int  checkid(int);
+int t(void);
+//void show_mouse(void);
+void Password();
+void issuerecord();
+void loaderanim();
+
+//list of global files that can be acceed form anywhere in program
+FILE *fp,*ft,*fs;
+
+
+COORD coord = {0, 0};
+//list of global variable
+int s;
+char findbook;
+char password[10]={"codewithc"};
+
+void gotoxy (int x, int y)
+{
+coord.X = x; coord.Y = y; // X and Y coordinates
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+struct meroDate
+{
+int mm,dd,yy;
+};
+struct books
+{
+int id;
+char stname[20];
+char name[20];
+char Author[20];
+int quantity;
+float Price;
+int count;
+int rackno;
+char *cat;
+struct meroDate issued;
+struct meroDate duedate;
+};
+struct books a;
+int main()
+{
+Password();
+getch();
+return 0;
+
+}
+void mainmenu()
+{
+//loaderanim();
+system("cls");
+//    textbackground(13);
+int i;
+gotoxy(20,3);
+printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2 MAIN MENU \xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+//    show_mouse();
+gotoxy(20,5);
+printf("\xDB\xDB\xDB\xDB\xB2 1. Add Books   ");
+gotoxy(20,7);
+printf("\xDB\xDB\xDB\xDB\xB2 2. Delete books");
+gotoxy(20,9);
+printf("\xDB\xDB\xDB\xDB\xB2 3. Search Books");
+gotoxy(20,11);
+printf("\xDB\xDB\xDB\xDB\xB2 4. Issue Books");
+gotoxy(20,13);
+printf("\xDB\xDB\xDB\xDB\xB2 5. View Book list");
+gotoxy(20,15);
+printf("\xDB\xDB\xDB\xDB\xB2 6. Edit Book's Record");
+gotoxy(20,17);
+printf("\xDB\xDB\xDB\xDB\xB2 7. Close Application");
+gotoxy(20,19);
+printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+gotoxy(20,20);
+t();
+gotoxy(20,21);
+printf("Enter your choice:");
+switch(getch())
+{
+case '1':
+addbooks();
+break;
+case '2':
+deletebooks();
+break;
+case '3':
+searchbooks();
+break;
+case '4':
+issuebooks();
+break;
+case '5':
+viewbooks();
+break;
+case '6':
+editbooks();
+break;
+case '7':
+{
+system("cls");
+gotoxy(16,3);
+printf("\tLibrary Management System");
+gotoxy(16,4);
+printf("\tMini Project in C");
+gotoxy(16,5);
+printf("\tis brought to you by");
+gotoxy(16,7);
+printf("\tCode with C Team");
+gotoxy(16,8);
+printf("******************************************");
+gotoxy(16,10);
+printf("*******************************************");
+gotoxy(16,11);
+printf("*******************************************");
+gotoxy(16,13);
+printf("********************************************");
+gotoxy(10,17);
+printf("Exiting in 3 second...........>");
+//flushall();
+Sleep(3000);
+exit(0);
+}
+default:
+{
+gotoxy(10,23);
+printf("\aWrong Entry!!Please re-entered correct option");
+if(getch())
+mainmenu();
+}
+
+}
+}
+void addbooks(void)    //funtion that add books
+{
+system("cls");
+int i;
+gotoxy(20,5);
+printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2SELECT CATEGOIES\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+gotoxy(20,7);
+printf("\xDB\xDB\xDB\xDB\xB2 1. Computer");
+gotoxy(20,9);
+printf("\xDB\xDB\xDB\xDB\xB2 2. Electronics");
+gotoxy(20,11);
+printf("\xDB\xDB\xDB\xDB\xB2 3. Electrical");
+gotoxy(20,13);
+printf("\xDB\xDB\xDB\xDB\xB2 4. Civil");
+gotoxy(20,15);
+printf("\xDB\xDB\xDB\xDB\xB2 5. Mechanical");
+gotoxy(20,17);
+printf("\xDB\xDB\xDB\xDB\xB2 6. Architecture");
+gotoxy(20,19);
+printf("\xDB\xDB\xDB\xDB\xB2 7. Back to main menu");
+gotoxy(20,21);
+printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+gotoxy(20,22);
+printf("Enter your choice:");
+scanf("%d",&s);
+if(s==7)
+
+mainmenu() ;
+system("cls");
+fp=fopen("Bibek.dat","ab+");
+if(getdata()==1)
+{
+a.cat=catagories[s-1];
+fseek(fp,0,SEEK_END);
+fwrite(&a,sizeof(a),1,fp);
+fclose(fp);
+gotoxy(21,14);
+printf("The record is sucessfully saved");
+gotoxy(21,15);
+printf("Save any more?(Y / N):");
+if(getch()=='n')
+mainmenu();
+else
+system("cls");
+addbooks();
+}
+}
+void deletebooks()    //function that delete items from file fp
+{
+system("cls");
+int d;
+char another='y';
+while(another=='y')  //infinite loop
+{
+system("cls");
+gotoxy(10,5);
+printf("Enter the Book ID to  delete:");
+scanf("%d",&d);
+fp=fopen("Bibek.dat","rb+");
+rewind(fp);
+while(fread(&a,sizeof(a),1,fp)==1)
+{
+if(a.id==d)
+{
+
+gotoxy(10,7);
+printf("The book record is available");
+gotoxy(10,8);
+printf("Book name is %s",a.name);
+gotoxy(10,9);
+printf("Rack No. is %d",a.rackno);
+findbook='t';
+}
+}
+if(findbook!='t')
+{
+gotoxy(10,10);
+printf("No record is found modify the search");
+if(getch())
+mainmenu();
+}
+if(findbook=='t' )
+{
+gotoxy(10,9);
+printf("Do you want to delete it?(Y/N):");
+if(getch()=='y')
+{
+ft=fopen("test.dat","wb+");  //temporary file for delete
+rewind(fp);
+while(fread(&a,sizeof(a),1,fp)==1)
+{
+if(a.id!=d)
+{
+fseek(ft,0,SEEK_CUR);
+fwrite(&a,sizeof(a),1,ft); //write all in tempory file except that
+}                              //we want to delete
+}
+fclose(ft);
+fclose(fp);
+remove("Bibek.dat");
+rename("test.dat","Bibek.dat"); //copy all item from temporary file to fp except that
+fp=fopen("Bibek.dat","rb+");    //we want to delete
+if(findbook=='t')
+{
+gotoxy(10,10);
+printf("The record is sucessfully deleted");
+gotoxy(10,11);
+printf("Delete another record?(Y/N)");
+}
+}
+else
+mainmenu();
+fflush(stdin);
+another=getch();
+}
+}
+gotoxy(10,15);
+mainmenu();
+}
